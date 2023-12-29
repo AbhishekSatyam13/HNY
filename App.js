7d46dda9bd18b1a9decc53dab1c111d3db479d63
AbhishekSatyam13 <abhishek.satyam@antwalk.com>
2023-12-29 21:20:31 +0530

    revert previous commit related to parcel

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const heading1 = React.createElement("h1",{id: 'first', style: {color: 'green'}}, "Namaste!!");
-const heading2 = React.createElement("h1",{}, ["React!!", React.createElement("h6",{}, "op")]);
-const container1 = React.createElement("div",{}, [heading1, heading2]);
-const container2 = React.createElement("div",{}, [container1, heading1]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container2);
--- practice.js
+++
@@ -1,9 +0,0 @@
-const x = () => {
-    for(let i=0;i<5;i++){
-        setTimeout(function(){
-            console.log(i);
-        },i*1000);
-    };
-    console.log("Namaste");
-};
-x();
